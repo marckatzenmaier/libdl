2130611b39d8eda11c2d1aea61db8f4eb87c45e2
MarcKatzenmaier <marc.katzenmaier@tum.de>
2019-06-18 21:59:13 +0200

    probable fix for clang error

--- include/libdl/math_functions.h
+++ include/libdl/math_functions.h
@@ -18,7 +18,7 @@ Eigen::Tensor<float, 4, Eigen::RowMajor> pool_average(const Eigen::Tensor<float,
 Eigen::Tensor<float, 4, Eigen::RowMajor> pool_average_backward(const Eigen::Tensor<float, 4, Eigen::RowMajor> &grad, int kernel_h, int kernel_w, int stride_h, int stride_w);
 Eigen::Tensor<float, 4, Eigen::RowMajor> nn_upscale(const Eigen::Tensor<float, 4, Eigen::RowMajor> &input, int height, int width);
 Eigen::Tensor<float, 4, Eigen::RowMajor> softmax(const Eigen::Tensor<float, 4, Eigen::RowMajor> &input);
-Eigen::Tensor<float, 4, Eigen::RowMajor> softmax_backward(const Eigen::Tensor<float, 4, Eigen::RowMajor> &input, const Eigen::Tensor<float, 4, Eigen::RowMajor> &grad);
+Eigen::Tensor<float, 4, Eigen::RowMajor> softmax_backward(Eigen::Tensor<float, 4, Eigen::RowMajor> &input, const Eigen::Tensor<float, 4, Eigen::RowMajor> &grad);
 
 Eigen::Tensor<float, 3, Eigen::RowMajor> diagonalize(Eigen::Tensor<float, 2, Eigen::RowMajor> &input);
 
